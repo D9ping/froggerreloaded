6efdb4b8f44820f658e7c11b8e8cf141349d500f
gertjanbuys@gmail.com <gertjanbuys@gmail.com@c051be08-d9f9-11de-8d17-8328ca00869f>
2010-01-11 17:38:31 +0000

    DBConnection.cs update

--- DBConnection.cs
+++ DBConnection.cs
@@ -8,17 +8,18 @@ namespace Frogger
 {
 	public static class DBConnection
 	{
-		public static SqlDataReader GetData(string query)
+
+		public static OleDbDataReader GetData(string query)
 		{
 			string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\highscores.accdb;Persist Security Info=False;";
 
-            SqlConnection myConnection = null;
-            SqlDataReader reader;
+            OleDbConnection myConnection = null;
+            OleDbDataReader reader;
             try
             {
-                myConnection = new SqlConnection(connectionString);
+                myConnection = new OleDbConnection(connectionString);
                 myConnection.Open();
-                SqlCommand command = new SqlCommand(query, myConnection);
+                OleDbCommand command = new OleDbCommand(query, myConnection);
                 reader = command.ExecuteReader();
             }
             finally
@@ -31,34 +32,16 @@ namespace Frogger
 
 		public static void SetData(string query)
 		{
-            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\highscores.accdb;Persist Security Info=False;";
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\highscores.mdb;Persist Security Info=False;";
 
-			SqlConnection myConnection = new SqlConnection(connectionString);
-			myConnection.Open();
+            OleDbConnection myConnection = new OleDbConnection(connectionString);
+            myConnection.Open();
 
-			SqlCommand command = new SqlCommand(query, myConnection);
+			OleDbCommand command = new OleDbCommand(query, myConnection);
 
 			command.ExecuteNonQuery();
 
 			myConnection.Close();
 		}
-
-        public static void VoegHighscoreToe(DateTime tijddatum, string naam, int speeltijd, int level)
-        {
-            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\highscores.accdb;Persist Security Info=False;";
-
-            SqlConnection myConnection = new SqlConnection(connectionString);
-            myConnection.Open();
-
-            //INSERT INTO table_name
-            //VALUES (value1, value2, value3,...)
-            string query = "INSERT INTO HIGHSCORES VALUES ('" + tijddatum.ToString() + "', '" + naam + "', '" + speeltijd.ToString() + "', '" + level.ToString() + "')";
-
-            SqlCommand command = new SqlCommand(query, myConnection);
-
-            command.ExecuteNonQuery();
-
-            myConnection.Close();
-        }
 	}
 }
